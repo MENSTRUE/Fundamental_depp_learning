965b0b9eea662c3f01a5ea395d9c5a008415b20c
MENSTRUE <menstruesalvation@gmail.com>
2025-10-15 22:11:47 +0700

    batch loading + test_preprocessin

---
+++ text_preprocessing/case_folding.py
@@ -0,0 +1,8 @@
+# Teks awal
+teks_asli = "Saya Sedang Belajar Natural Language Processing (NLP)."
+
+# Proses Case Folding
+teks_lowercase = teks_asli.lower()
+
+print("Teks Asli:", teks_asli)
+print("Hasil Case Folding:", teks_lowercase)
